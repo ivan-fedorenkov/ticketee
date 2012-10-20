fc386fdb8e7154a9d911b241cdb785c741d5a766
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-10-20 06:16:40 -0700

    Implemented page title helper

--- features/create_project.feature
+++ features/create_project.feature
@@ -5,5 +5,7 @@ Feature: Creating projects
 
 Scenario: Creating a project
   Given I am on the homepage
-  When I create a new project
+  When I create a new project "Text Mate2"
   Then I should see "Project has been created."
+  And I should be on the project page for "Text Mate2"
+  And I should see "Text Mate2 - Projects - Ticketee"
--- features/step_definitions/create_project_steps.rb
+++ features/step_definitions/create_project_steps.rb
@@ -2,13 +2,17 @@ Given /^I am on the homepage$/ do
   visit('/')
 end
 
-When /^I create a new project$/ do
+When /^I create a new project "(.*)"$/ do |project_name|
   click_link 'New Project'
-  fill_in 'Name', :with => 'Text Mate2'
+  fill_in 'Name', :with => project_name
   click_button 'Create Project'
 end
 
 Then /^I should see "(.*?)"$/ do |message|
-  page.has_content?(message)
+  page.has_content?(message).should be_true
+end
+
+Then /^I should be on the project page for "(.*?)"$/ do |project_name|
+  current_path.should == project_path(Project.find_by_name!(project_name))
 end
 

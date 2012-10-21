036ae1a05e2eb747a2c7f2eccecde968f969f9cb
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-10-21 12:49:25 -0700

    Implemented update projects functionality

--- features/step_definitions/create_project_steps.rb
+++ features/step_definitions/create_project_steps.rb
@@ -3,7 +3,3 @@ When /^I create a new project "(.*)"$/ do |project_name|
   fill_in 'Name', :with => project_name
   click_button 'Create Project'
 end
-
-Then /^I should see "(.*?)"$/ do |message|
-  page.has_content?(message).should be_true
-end

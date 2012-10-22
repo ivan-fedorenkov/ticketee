de318aa072e7616b72390d3dec9bb9ee2e67c28c
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-10-22 11:19:50 -0700

    forgot to add something

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :project
+  belongs_to :projecs
   validates :title, :presence => true
   validates :description, :presence => true, :length => {:minimum => 10}
 end

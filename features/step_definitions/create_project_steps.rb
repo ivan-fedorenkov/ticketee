ee311d151923ab0efd1690b4404607289ea18659
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-10-24 12:01:28 -0700

    Clean up some trash

--- features/step_definitions/create_project_steps.rb
+++
@@ -1,5 +0,0 @@
-When /^I create a new project "(.*)"$/ do |project_name|
-  click_link 'New Project'
-  fill_in 'Name', :with => project_name
-  click_button 'Create Project'
-end
--- features/step_definitions/view_project_steps.rb
+++
@@ -1,4 +0,0 @@
-Given /^there is a project called "(.*?)"$/ do |name|
-  @project = FactoryGirl.create(:project, :name => name)
-end
-

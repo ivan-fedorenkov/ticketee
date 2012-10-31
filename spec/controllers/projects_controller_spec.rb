7578d07bf3f86970a6d961542488acac93393170
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-10-31 08:30:49 -0700

    todo: section 8.3.1

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -9,6 +9,7 @@ describe ProjectsController do
   end
 
   it "displays an error for a missing project" do
+    sign_in(:user, user)
     get :show, :id => 'Not-here'
     response.should redirect_to(projects_path)
     message = "The project you were looking for could not be found."
@@ -28,6 +29,15 @@ describe ProjectsController do
         flash[:alert].should eql("You must be an admin to do that.")
       end
     end
+
+
+    it "cannot access the show action" do
+      sign_in(:user, user)
+      get :show, :id => project.id
+      response.should redirect_to(projects_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+
   end
 
 end

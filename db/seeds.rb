2b85b3964a54a44e8871479177a3e884eb471635
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-06 00:50:22 -0800

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin_user = User.create(
+    :email => "admin@ticketee.com",
+    :password => "password",
+    :confirm_password => "password")
+admin_user.admin = true
+admin_user.confirm!
+
+Project.create(:name => "Ticketee beta")

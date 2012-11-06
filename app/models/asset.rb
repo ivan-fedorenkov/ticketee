3b5723a76d8a1f37904d487aa70be463aea2c674
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-06 07:03:52 -0800

    Assets are now strictly served through FilesController

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,6 @@
 class Asset < ActiveRecord::Base
-  has_attached_file :asset
+  belongs_to :ticket
+  has_attached_file :asset, 
+      :path => ":rails_root/files/:filename",
+      :url => "/files/:filename"
 end

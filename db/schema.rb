12b9afe197d128c68e6b4b74af937575c826aacc
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-10 12:44:31 -0800

    Display a comment's state transition

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110162536) do
+ActiveRecord::Schema.define(:version => 20121110200847) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121110162536) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "state_id"
+    t.integer  "previous_state_id"
   end
 
   add_index "comments", ["ticket_id"], :name => "index_comments_on_ticket_id"

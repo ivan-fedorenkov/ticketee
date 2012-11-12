caca33a872252a887d62c2adbd39c7f6635c0c00
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2012-11-12 03:14:58 -0800

    Admins can now set a default state for tickets

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,4 +2,16 @@ class State < ActiveRecord::Base
   def to_s
     self.name
   end
+
+  def default!
+    current_default_state = State.find_by_default(true)
+
+    self.default = true
+    self.save!
+
+    if current_default_state
+      current_defult_state.default = false
+      current_defult_state.save!
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110200847) do
+ActiveRecord::Schema.define(:version => 20121112111024) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -52,9 +52,10 @@ ActiveRecord::Schema.define(:version => 20121110200847) do
   end
 
   create_table "states", :force => true do |t|
-    t.string "name"
-    t.string "color"
-    t.string "background"
+    t.string  "name"
+    t.string  "color"
+    t.string  "background"
+    t.boolean "default",    :default => false
   end
 
   create_table "tickets", :force => true do |t|

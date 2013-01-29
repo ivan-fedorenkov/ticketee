5888d7fd35e75abfa978f01ae0579d3634e8615f
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2013-01-29 22:45:06 +0400

    Implement v2 of the API renaming name to title for projects

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Ticketee::Application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v2 do
+      resources :projects do
+        resources :tickets
+      end
+    end
+  end
+
   get "comments/create"
 
   devise_for :users, :controllers => {:registrations => "registrations"}
---
+++ spec/apis/v2/projects_spec.rb
@@ -0,0 +1,124 @@
+require 'spec_helper'
+
+describe '/api/v2/projects', :type => :api do
+  
+  let(:user) { create_user! }
+  let(:token) { user.authentication_token }
+  
+  before do
+    @project = FactoryGirl.create(:project)
+    user.permissions.create!(:action => "view", :thing => @project)
+  end
+  
+  context "projects viewable by this user" do
+
+    before { FactoryGirl.create(:project, :name => "Access Denied") }
+
+    let(:url) { "/api/v2/projects" }
+    let(:options) { {:except => :name, :methods => :title} }
+    
+    it "json" do
+      get "#{url}.json", :token => token
+      projects_json = Project.for(user).all.to_json(options)
+      last_response.body.should eql(projects_json)
+      last_response.status.should eql(200)
+      
+      projects = JSON.parse(last_response.body)
+      
+      projects.any? { |p| p["title"] == "Ticketee" }.should be_true
+      projects.any? { |p| p["name"].blank? }.should be_true
+      projects.any? { |p| p["title"] == "Access Denied" }.should be_false
+
+    end
+
+    it "xml" do
+      get "#{url}.xml", :token => token
+      last_response.body.should eql(Project.readable_by(user).to_xml(options))
+      projects = Nokogiri::XML(last_response.body)
+      projects.css("project title").text.should eql("Ticketee")
+      projects.css("project name").text.should eql("")
+    end
+
+  end
+
+  context "creating a project" do
+
+    before(:each) do
+      user.admin = true
+      user.save!
+    end
+
+    let(:url) { "/api/v2/projects" }
+
+    it "successful JSON" do
+      post "#{url}.json", :token => token, :project => {:name => "Inspector"}
+      project = Project.find_by_name("Inspector")
+      route = "/api/v2/projects/#{project.id}"
+
+      last_response.status.should eql(201)
+      last_response.headers["Location"].should eql(route)
+      last_response.body.should eql(project.to_json)
+    end
+
+    it "unsuccessful JSON" do
+      post "#{url}.json", :token => token, :project => {}
+      last_response.status.should eql(422)
+      errors = {"name" => ["can't be blank"]}.to_json
+      last_response.body.should eql(errors)
+    end
+  end
+
+  context "viewing a project" do
+    let(:url) { "/api/v2/projects/#{@project.id}" }
+
+    before do
+      FactoryGirl.create(:ticket, :project => @project)
+    end
+
+    it "json" do
+      get "#{url}.json", :token => token
+      project = @project.to_json(:methods => "last_ticket")
+      last_response.body.should eql(project)
+      last_response.status.should eql(200)
+
+      project_response = JSON.parse(last_response.body)
+
+      ticket_title = project_response["last_ticket"]["title"]
+      ticket_title.should_not be_blank
+    end
+  end
+
+  context "editing a project" do
+    before { user.admin = true; user.save! }
+    let(:url) { "/api/v2/projects/#{@project.id}" }
+
+    it "successful json" do
+      put "#{url}.json", :token => token, :project => { :name => "Not Ticketee" }
+      last_response.status.should eql(200)
+      @project.reload
+      @project.name.should eql("Not Ticketee")
+      last_response.body.should eql("{}")
+    end
+
+    it "unsuccessful json" do
+      put "#{url}.json", :token => token, :project => { :name => "" }
+      last_response.status.should eql(422)
+      @project.reload
+      @project.name.should eql("Ticketee")
+      errors = { :name => ["can't be blank"] }
+      last_response.body.should eql(errors.to_json)
+    end
+  end
+
+  context "deleting a project" do
+    before { user.admin = true; user.save }
+    let(:url) { "/api/v2/projects/#{@project.id}" }
+
+    it "json" do
+      delete "#{url}.json", :token => token
+      last_response.status.should eql(200)
+    end
+  end
+end
+
+
